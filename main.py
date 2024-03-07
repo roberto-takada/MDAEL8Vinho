761895987f23a634c6f1e9075fabdf66bc600194
roberto-takada <149258321+roberto-takada@users.noreply.github.com>
2024-03-07 11:36:43 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
 import pandas as pd
-import numpy as np
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 def main():
 
     arquivo_entrada = 'winequality-all.csv'
     arquivo_saida = 'winequality-all-clean.csv'
     nomes_das_colunas = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality', 'type']
-    colunas_a_utilizar = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', "free sulfur dioxide", 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality', 'type']
+    colunas_a_utilizar = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality', 'type']
     dados_na_memoria = pd.read_csv(arquivo_entrada, names=nomes_das_colunas, usecols=colunas_a_utilizar, na_values='?')
 
-    print('PRIMEIRAS 10 LINHAS DO ARQUIVO \n')
-    print(dados_na_memoria.head(10))
-    print('\n')
-
-    print('INFORMAÇÕES DOS DADOS: \n')
-    print(dados_na_memoria.info())
-    print('\n')
-
-    print('DESCRIÇÃO DOS DADOS: \n')
-    print(dados_na_memoria.describe())
-    print('\n')
+    MostraInformacoesDoDataFrame(dados_na_memoria)
 
     print('VALORES FALTANTES: \n')
     print(dados_na_memoria.isnull().sum())
     print('\n')
 
-    dados_na_memoria.to_numeric()
-    mudanca = {'red': 1, 'white': 2}
-    dados_na_memoria.replace({'red': mudanca, 'white': mudanca})
+    dados_na_memoria.to_csv(arquivo_saida, header=False, index=False)
 
+    arquivo_entrada = 'winequality-all-clean.csv'
+    arquivo_saida = 'winequality-all-normalized.csv'
+    nomes_das_colunas = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
+    valores_independentes = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+    valores_dependentes = 'quality'
+    dados_na_memoria = pd.read_csv(arquivo_entrada, names= nomes_das_colunas)
+
+    x = dados_na_memoria.loc[:, valores_independentes].values
+    y = dados_na_memoria.loc[:, [valores_dependentes]].values
+
+    #Normalização com Z-score
+    x_zscore = StandardScaler().fit_transform(x)
+    dataframe_normalizado_zscore = pd.DataFrame(data = x_zscore, columns = valores_independentes)
+    #dataframe_normalizado_zscore = pd.concat([dataframe_normalizado_zscore, dados_na_memoria[[valores_dependentes]]], axis = 1)
+    MostraInformacoesDoDataFrame(dataframe_normalizado_zscore)
+
+    #Normalização Min-Max
+    x_minmax = MinMaxScaler().fit_transform(x)
+    dataframe_normalizado_minmax = pd.DataFrame(data = x_minmax, columns = valores_independentes)
+    #dataframe_normalizado_minmax = pd.concat([dataframe_normalizado_minmax, dados_na_memoria[[valores_dependentes]]], axis = 1)
+    MostraInformacoesDoDataFrame(dataframe_normalizado_minmax)
+
+
+def MostraInformacoesDoDataFrame(dados_na_memoria):
     print('PRIMEIRAS 10 LINHAS DO ARQUIVO \n')
     print(dados_na_memoria.head(10))
     print('\n')
-    # dados_na_memoria.to_csv(arquivo_saida, header=False, index=False)
+
+    print('INFORMACOES DOS DADOS: \n')
+    print(dados_na_memoria.info())
+    print('\n')
+
+    print('DESCRICAO DOS DADOS: \n')
+    print(dados_na_memoria.describe())
+    print('\n')
 
 
 if __name__ == "__main__":
