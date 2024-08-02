a2658e114cdcd6f4cd8825ce98aff32430b2e877
roberto-takada <roberto.takadamm@gmail.com>
2024-08-02 16:23:03 -0300

    Codigo refatorado

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MDAEL8Vinho
+# Mineração de Dados
+## Trabalho: Qualidade do Vinho
+
+### Terminar : Algoritmo KNN
+### Começar : KMédias, SVM, Regressão, GMM, Arvore de Decisão.
--- main.py
+++ main.py
@@ -1,61 +1,45 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
+
+from preprocessing.calculo_variancias import calculo_variancias
+from preprocessing.normalizacao_minmax import minmax
+from preprocessing.normalizacao_zscore import z_score
+from preprocessing.print_dados import print_dados
+from preprocessing.print_graficos import print_matriz_correlacao, print_grafico_2variaveis
+from preprocessing.projecao_pca import projecao_pca
+
 
 def main():
-
-    arquivo_entrada = 'winequality-white.csv'
-    arquivo_saida = 'winequality-white-clean.csv'
-    nomes_das_colunas = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
-    colunas_a_utilizar = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
-    dados_na_memoria = pd.read_csv(arquivo_entrada, names=nomes_das_colunas, usecols=colunas_a_utilizar, na_values='?')
-
-    MostraInformacoesDoDataFrame(dados_na_memoria)
-
-    print('VALORES FALTANTES: \n')
-    print(dados_na_memoria.isnull().sum())
-    print('\n')
-
-    dados_na_memoria.to_csv(arquivo_saida, header=False, index=False)
-
-    arquivo_entrada = 'winequality-white-clean.csv'
-    arquivo_saida = 'winequality-white-normalized.csv'
-    nomes_das_colunas = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
-    valores_independentes = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+    # COMENTAR AQUILO QUE NÃO FOR USAR
+    # Lembrar de mudar as variáveis para vinho vermelho ou vinho branco
+    arquivo_entrada = './database/winequality-white-clean.csv'
+    arquivo_entrada_normalizado = './database/winequality-white-normalized.csv'
+    arquivo_saida_normalizado = './database/winequality-white-normalized.csv'
+    nomes_das_colunas = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                         'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol',
+                         'quality']
+    valores_independentes = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                             'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
     valores_dependentes = 'quality'
-    dados_na_memoria = pd.read_csv(arquivo_entrada, names= nomes_das_colunas)
+    variavel1 = 'fixed acidity'
+    variavel2 = 'free sulfur dioxide'
 
-    x = dados_na_memoria.loc[:, valores_independentes].values
-    y = dados_na_memoria.loc[:, [valores_dependentes]].values
+    dados = pd.read_csv(arquivo_entrada, names=nomes_das_colunas, usecols=nomes_das_colunas, na_values='?')
+    print_dados(dados)
+    print_grafico_2variaveis(dados, variavel1, variavel2)
 
-    #Normalização com Z-score
-    x_zscore = StandardScaler().fit_transform(x)
-    dataframe_normalizado_zscore = pd.DataFrame(data = x_zscore, columns = valores_independentes)
-    dataframe_variaveis_dependentes = pd.DataFrame(data = y, columns = [valores_dependentes])
-    dataframe_normalizado_zscore = pd.concat([dataframe_normalizado_zscore, dataframe_variaveis_dependentes], axis = 1)
-    MostraInformacoesDoDataFrame(dataframe_normalizado_zscore)
+    # Calculo das Variancias
+    calculo_variancias(dados, 2)
+    dados = pd.read_csv(arquivo_entrada, names=nomes_das_colunas, usecols=nomes_das_colunas, na_values='?')
+    print_matriz_correlacao(dados)
 
-    #Normalização Min-Max
-    x_minmax = MinMaxScaler().fit_transform(x)
-    dataframe_normalizado_minmax = pd.DataFrame(data = x_minmax, columns = valores_independentes)
-    dataframe_variaveis_dependentes = pd.DataFrame(data = y, columns = [valores_dependentes])
-    dataframe_normalizado_minmax = pd.concat([dataframe_normalizado_minmax, dataframe_variaveis_dependentes], axis = 1)
-    MostraInformacoesDoDataFrame(dataframe_normalizado_minmax)
-    dataframe_normalizado_minmax.to_csv(arquivo_saida, header=False, index=False)
+    # NORMALIZAÇÃO
+    minmax(dados, valores_independentes, valores_dependentes, arquivo_saida_normalizado)
+    z_score(dados, valores_independentes, valores_dependentes, arquivo_saida_normalizado)
 
-
-def MostraInformacoesDoDataFrame(dados_na_memoria):
-    print('PRIMEIRAS 10 LINHAS DO ARQUIVO \n')
-    print(dados_na_memoria.head(10))
-    print('\n')
-
-    print('INFORMACOES DOS DADOS: \n')
-    print(dados_na_memoria.info())
-    print('\n')
-
-    print('DESCRICAO DOS DADOS: \n')
-    print(dados_na_memoria.describe())
-    print('\n')
+    # PROJECAO PCA 2 E 3 COMPONENTES
+    # Mudar o 0 para 1 se precisar da projeção PCA para 3 componentes
+    dados = pd.read_csv(arquivo_entrada_normalizado, names=nomes_das_colunas)
+    projecao_pca(dados, valores_independentes, valores_dependentes, 1)
 
 
 if __name__ == "__main__":
